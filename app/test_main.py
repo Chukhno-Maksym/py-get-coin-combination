25d35171201525649c71956d1173a34d4ec367e8
Maksym <chukhno.maksym@gmail.com>
2024-12-18 22:16:48 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -9,8 +9,7 @@ from app.main import get_coin_combination
         (6, [1, 1, 0, 0]),
         (17, [2, 1, 1, 0]),
         (50, [0, 0, 0, 2]),
-        (100, [0, 0, 0, 4])
     ],
 )
-def combination(cents: int, result: list) -> None:
+def test_combination(cents: int, result: list) -> None:
     assert get_coin_combination(cents) == result
